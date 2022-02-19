2fd4803e8e7990d540f35662e211ca57cab2e0a5
hiệp hoàng <18130077@st.hcmuaf.edu.vn>
2022-02-19 10:51:57 +0700

    ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home'
+import Services from './components/pages/Sevices'
+import Product from './components/pages/Product'
+import SignUp from './components/pages/SignUP'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/services' component={Services}/>
+            <Route path='/products' component={Product}/>
+            <Route path='/sign-up' component={SignUp}/>
+        </Switch>
+      </Router>
+    </>
+             
+    
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import '../../App.css'
+import HeroSection from '../HeroSection.js'
+import Cards from '../Cards.js'
+import Footer from '../Footer.js'
+function Home(){
+    return (
+        <>
+          <HeroSection/>
+          <Cards/>
+          <Footer/>
+        </>
+    )
+}
+export default Home
